9427a247d628db00d442d9d54af63e0a443c5326
leesc22 <leesiewching22@gmail.com>
2017-10-12 16:44:07 +0800

    User can sign in with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  root 'users#show'
+  root 'sessions#new'
+
+  # Redirect user from Facebook login
+  get '/auth/:provider/callback' => 'sessions#create_from_omniauth'
 
   get '/sign_in', to: 'sessions#new'
   post '/sign_in', to: 'sessions#create'
   get '/sign_out', to: 'sessions#destroy'
 
   get '/sign_up', to: 'users#new'
-  resources :users, only: [:create, :show]
+  resources :users, only: [:show, :edit, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012035456) do
+ActiveRecord::Schema.define(version: 20171012075648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.string "uid"
+    t.string "token"
+    t.string "provider"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -24,4 +34,5 @@ ActiveRecord::Schema.define(version: 20171012035456) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "authentications", "users"
 end
