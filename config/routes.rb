c134702812b23a9f749793972f69f52c77ee6132
leesc22 <leesiewching22@gmail.com>
2017-10-12 13:09:14 +0800

    Set up user sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'users#show'
 
+  get '/sign_in', to: 'sessions#new'
+  post '/sign_in', to: 'sessions#create'
+  get '/sign_out', to: 'sessions#destroy'
+
   get '/sign_up', to: 'users#new'
   post '/users', to: 'users#create'
 end
