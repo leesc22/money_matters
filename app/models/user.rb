c57c95e80a7567986f574b0b53f5f66beb39a239
leesc22 <leesiewching22@gmail.com>
2017-10-12 12:36:36 +0800

    Set up user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+	validates :name, :email, :password, :password_confirmation, presence: true
+	validates :email, uniqueness: true
+	validates :password, length: { is: 8 }
+	validates :password, confirmation: true
+	has_secure_password
 end
