ceb6dca5589c2239d22e490051c5bca6726f48e6
leesc22 <leesiewching22@gmail.com>
2017-10-13 17:24:07 +0800

    User can search for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ApplicationRecord
 	mount_uploader :image, ArticleImageUploader
+
+	def self.search(search)
+    if search
+      where("title ILIKE :search", search: "%#{search}%")
+    else
+      all
+    end
+  end
 end
