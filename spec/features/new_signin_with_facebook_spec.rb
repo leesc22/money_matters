a707edb007cff7fd62b9f3fcae23e1168b001559
leesc22 <leesiewching22@gmail.com>
2017-10-14 22:08:38 +0800

    Remove auto generated rspec example

--- spec/features/new_signin_with_facebook_spec.rb
+++ spec/features/new_signin_with_facebook_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Sign In", type: :feature do
 		visit root_path
 		expect(page).to have_css('a', text: 'SIGN IN')
 		click_link 'SIGN IN'
-		expect(page.current_path).to eq signin_path
+		expect(page.current_path).to eq sign_in_path
 		click_link 'Continue with Facebook'
 		expect(page.current_path).to eq user_path
 	end
--- spec/models/investment_spec.rb
+++ spec/models/investment_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe Investment, type: :model do
 	let(:regular_amount)       { 200 }
 	let(:regular_period)       { 'monthly' }
 	let(:compounding_period)   { 'yearly' }
+	let(:valid_investment) { Investment.new(initial_amount: initial_amount, interest_rate: interest_rate, interest_rate_period: interest_rate_period, period: period, period_type: period_type, regular_amount: regular_amount, regular_period: regular_period, compounding_period: compounding_period) }
+	let(:missing_interest_rate) { Investment.new(initial_amount: initial_amount, interest_rate_period: interest_rate_period, period: period, period_type: period_type, regular_amount: regular_amount, regular_period: regular_period, compounding_period: compounding_period) }
+	let(:missing_period) { Investment.new(initial_amount: initial_amount, interest_rate: interest_rate, interest_rate_period: interest_rate_period, period_type: period_type, regular_amount: regular_amount, regular_period: regular_period, compounding_period: compounding_period) }
 
   context "validations" do
   	describe "validates interest_rate and period" do
-  		it { is_expected.to validate_presence_of(:interest_rate) }
-  		it { is_expected.to validate_presence_of(:period) }
-
   		it "validates interest_rate to be a number" do
 	  		expect(:interest_rate).to be_kind_of(Numeric)
   		end
@@ -23,19 +23,16 @@ RSpec.describe Investment, type: :model do
 	  describe "validates attributes" do
 		  # happy path
 		  it "can be created when all attributes are present" do
-		  	let(:investment) { Investment.new(initial_amount: initial_amount, interest_rate: interest_rate, interest_rate_period: interest_rate_period, period: period, period_type: period_type, regular_amount: regular_amount, regular_period: regular_period, compounding_period: compounding_period) }
-		  	expect(investment.save).to eq(true)
+		  	expect(valid_investment.save).to eq(true)
 		  end
 
 		  # unhappy path
 		  it "cannot be created without an interest_rate " do
-		  	let(:investment) { Investment.new(initial_amount: initial_amount, interest_rate_period: interest_rate_period, period: period, period_type: period_type, regular_amount: regular_amount, regular_period: regular_period, compounding_period: compounding_period) }
-		  	expect(investment.save).to eq(false)
+		  	expect(missing_interest_rate.save).to eq(false)
 		  end
 
 		  it "cannot be created without a period " do
-		  	let(:investment) { Investment.new(initial_amount: initial_amount, interest_rate: interest_rate, interest_rate_period: interest_rate_period, period_type: period_type, regular_amount: regular_amount, regular_period: regular_period, compounding_period: compounding_period) }
-		  	expect(investment.save).to eq(false)
+		  	expect(missing_period.save).to eq(false)
 		  end
 		end
   end
