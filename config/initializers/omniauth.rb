0190d9060cc5387de6fc134d90d6e8214036e421
leesc22 <leesiewching22@gmail.com>
2017-10-15 21:29:47 +0800

    Display cash flow in investment show page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], scope: 'email, public_profile', info_fields: 'name, email'
+  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], scope: 'email, public_profile', info_fields: 'name, email', display: 'popup', client_options: { ssl: { ca_file: '/usr/lib/ssl/certs/ca-certificates.crt' }}
 end
