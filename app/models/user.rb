1808e3cdcc71897ee2ecca6b846f0a281b155982
leesc22 <leesiewching22@gmail.com>
2017-10-16 16:50:51 +0800

    Complete data modelling for account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
 	validates :password, length: { is: 8 }, confirmation: true, allow_nil: true
   enum role: [:client, :moderator, :admin, :superadmin]
   mount_uploader :avatar, AvatarUploader
+  # before_save :downcase_email
+  # before_create :create_activation_digest
 
 	def self.create_with_auth_and_hash(authentication, auth_hash)
     user = self.create!(
@@ -25,5 +27,29 @@ class User < ApplicationRecord
   def fb_token
     x = self.authentications.find_by(provider: 'facebook')
     return x.token unless x.nil?
+  end
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  private
+
+  # Converts email to all lower-case.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
