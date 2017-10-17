92e0d081b9810057fedc03b91375d59b4f8246d0
leesc22 <leesiewching22@gmail.com>
2017-10-17 16:04:59 +0800

    Update migration file for account activation

--- db/migrate/20171016083152_add_activation_to_users.rb
+++ db/migrate/20171016083152_add_activation_to_users.rb
@@ -3,5 +3,6 @@ class AddActivationToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :activation_digest, :string
     add_column :users, :activated, :boolean, default: false
     add_column :users, :activated_at, :datetime
+    User.update_all(activated: true, activated_at: DateTime.now)
   end
 end
