0beeee458f19512b8ce05e6c5a467a54932d1a35
leesc22 <leesiewching22@gmail.com>
2017-10-19 14:50:15 +0800

    Include test for default scope

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   let(:user) { FactoryGirl.create(:user) }
   let(:article) { user.articles.new(title: 'Importance of Savings', content: 'Buffer for emergency funds') }
+  let(:article2) { user.articles.new(title: 'Type of Savings', content: 'FD and unit trust') }
 
   context "validations" do
 	  describe "do not have validations" do
@@ -10,6 +11,15 @@ RSpec.describe Article, type: :model do
 		  it "article can be created" do
 		  	expect(article.save).to eq(true)
 		  end
+		end
+	end
+
+	# test for default scope
+	describe 'default scope' do
+		it 'order by created at descending' do
+			article.save
+			article2.save
+			expect(Article.all).to eq([article2, article])
 		end
 	end
 
